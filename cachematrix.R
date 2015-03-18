92f7b53688fe925d4881d1178eb8e2951d4981fd
solaco <solaco@gmail.com>
2015-03-17 22:52:11 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function returns a list containing the following functions:
+## 1) set - sets the value of the matrix
+## 2) get - returns the value of the vector
+## 3) setSolve - sets the inverse of the matrix
+## 4) getSolve - returns the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setSolve <- function(solve) m <<- solve
-  getSolve <- function() m
-  list(set = set, get = get,
-       setSolve = setSolve,
-       getSolve = getSolve)
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) m <<- solve
+    getSolve <- function() m
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the matrix created with tmakeCacheMatrix
+## It first checks to see if the inverse has already been calculated. If so,
+## it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and sets the value of 
+## the matrix in the cache via the setSolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getSolve()
-  if(!is.null(m)) {
-    message("getting cached matrix")
-    return(m)
-  }
-  mtrx <- x$get()
-  m <- solve(mtrx, ...)
-  x$setSolve(m)
-  m
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getSolve()
+    if(!is.null(m)) {
+        message("getting cached matrix")
+        return(m)
+    }
+    mtrx <- x$get()
+    m <- solve(mtrx, ...)
+    x$setSolve(m)
+    m
 }
